522c808e78be2404dee9b3e66d9c84c42fdfe5a0
saltyp <struligt@gmail.com>
2024-03-13 12:47:05 +0100

    Improve with O(lgN) binary search in getCredits.

--- imdb.cc
+++ imdb.cc
@@ -5,6 +5,7 @@ using namespace std;
 #include <fcntl.h>
 #include <unistd.h>
 #include "imdb.h"
+#include <algorithm>
 
 const char *const imdb::kActorFileName = "actordata";
 const char *const imdb::kMovieFileName = "moviedata";
@@ -31,37 +32,44 @@ bool imdb::getCredits(const string& player, vector<film>& films) const {
   int* p_actorFile_start = (int*)actorFile;
   int num_actors = *p_actorFile_start; // first record of the file
   int* p_actor_offsets_start = p_actorFile_start + 1; // start of the actor offsets
-
-  // ! TODO : change to binary search O(lgN) from linear search O(N)
-  for (int i = 0; i < num_actors; i++) {
-    // get the actor record by pointing required num bytes past beginning of file:
-    char* p_actor_record_start = (char*)actorFile + *(p_actor_offsets_start + i); 
-    string actor_name = p_actor_record_start; // get the actor name
-    if (actor_name == player) {
-      was_found = true;
-      int actor_name_byte_length = sizeof(char)*actor_name.length();
-      int actor_nummovies_byte_offset =  (actor_name_byte_length + 1) % 2 == 0 ? (actor_name_byte_length + 1) : (actor_name_byte_length + 2);
-      int num_actor_movies = *(short*)(p_actor_record_start + actor_nummovies_byte_offset);
-      int byte_offset_to_movieFile_offsets = actor_nummovies_byte_offset + sizeof(short);
-      byte_offset_to_movieFile_offsets = byte_offset_to_movieFile_offsets % 4 == 0 ? byte_offset_to_movieFile_offsets : byte_offset_to_movieFile_offsets + 2;
-      int* p_movieFile_offsets = (int*)(p_actor_record_start + byte_offset_to_movieFile_offsets); // CHECK
-      // Fill in the films vector, by using the offsets stored in the actor_record to reference the movieFile
-      // whose records contain (i) c-string of movie name, (ii) single byte rep of (movie_year - 1900), (iii) extra null padding if previous byte-amount is odd, 
-      // (iv) short rep of num_actors, (v) possible 2 additonaly bytes of zero padding, then array of 4-byte int offsets into actorFile for each actor in the movie
-      for (int j = 0; j < num_actor_movies; j++) {
-        int movie_offset = *(p_movieFile_offsets + j);
-        char* p_movie_record_start = (char*)movieFile + movie_offset;
-        string movie_name = p_movie_record_start;
-        int movie_name_byte_length = sizeof(char)*movie_name.length();
-        int movie_year = *(char*)(p_movie_record_start + movie_name_byte_length + 1)+1900;
-        film movie = {movie_name, movie_year};
-        films.push_back(movie);
-      }
-      break;
+  
+  // binary search for the actor name:
+  auto cmp = [p_actorFile_start](const int offset, const string& actor_name_to_find) {
+    char* an_actor = (char*)p_actorFile_start + offset;
+    return (an_actor < actor_name_to_find);
+  };
+  int* p_found_actor_offset = std::lower_bound(p_actor_offsets_start, (p_actor_offsets_start+num_actors), player, cmp);
+  // get record of the actor if found:
+  if (p_found_actor_offset != (p_actor_offsets_start+num_actors)) {
+    was_found = true;
+    // go to the actor record by pointing required num bytes past beginning of file:
+    char* p_actor_record_start = (char*)actorFile + *p_found_actor_offset; 
+    string actor_name = p_actor_record_start; // get the actor name == player 
+    // get actor name & number of movies : 
+    int actor_name_byte_length = sizeof(char)*actor_name.length();
+    int actor_nummovies_byte_offset =  (actor_name_byte_length + 1) % 2 == 0 ? (actor_name_byte_length + 1) : (actor_name_byte_length + 2);
+    int num_actor_movies = *(short*)(p_actor_record_start + actor_nummovies_byte_offset);
+    int byte_offset_to_movieFile_offsets = actor_nummovies_byte_offset + sizeof(short);
+    byte_offset_to_movieFile_offsets = byte_offset_to_movieFile_offsets % 4 == 0 ? byte_offset_to_movieFile_offsets : byte_offset_to_movieFile_offsets + 2;
+    int* p_movieFile_offsets = (int*)(p_actor_record_start + byte_offset_to_movieFile_offsets); // CHECK
+    // Fill in the films vector, by using the offsets stored in the actor_record to reference the movieFile
+    // whose records contain (i) c-string of movie name, (ii) single byte rep of (movie_year - 1900), (iii) extra null padding if previous byte-amount is odd, 
+    // (iv) short rep of num_actors, (v) possible 2 additonaly bytes of zero padding, then array of 4-byte int offsets into actorFile for each actor in the movie
+    for (int j = 0; j < num_actor_movies; j++) {
+      int movie_offset = *(p_movieFile_offsets + j);
+      char* p_movie_record_start = (char*)movieFile + movie_offset;
+      string movie_name = p_movie_record_start;
+      int movie_name_byte_length = sizeof(char)*movie_name.length();
+      // 1 byte for stored (year-1900) to which is added 1900 to give year again :
+      int movie_year = *(char*)(p_movie_record_start + movie_name_byte_length + 1)+1900;
+      film movie = {movie_name, movie_year};
+      films.push_back(movie);
     }
   }
-  return was_found; 
+  return was_found;
 }
+
+
 bool imdb::getCast(const film& movie, vector<string>& players) const { return false; }
 
 imdb::~imdb()
